5c1fc9d3a7f2aa7d748e6b06cb5947a13ebea4dc
sumaiya-cse <223010412@eastdelta.edu.bd>
2025-07-05 21:20:57 +0600

    checking sumaiya user

--- backend/src/books/book.controller.js
+++ backend/src/books/book.controller.js
@@ -6,6 +6,7 @@ const postABook = async (req, res) => {
         await newBook.save();
         res.status(200).send({message: "Book posted successfully", book: newBook})
     } catch (error) {
+        
         console.error("Error creating book", error);
         res.status(500).send({message: "Failed to create book"})
     }
